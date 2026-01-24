68aa852d7a1c7cbd5519db3f14c81614fb1a3d98
Glenn Dunnegan <glenn.dunnegan@gmail.com>
2026-01-23 18:29:03 -0600

    edited scripts, added deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/Freelancefinance/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
