229b94b3ccd466a695add0fd593430ceba79db3f
Glenn Dunnegan <glenn.dunnegan@gmail.com>
2026-01-26 16:32:48 -0600

    vite config adjust

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/',
+    base: "/Freelancefinance",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
