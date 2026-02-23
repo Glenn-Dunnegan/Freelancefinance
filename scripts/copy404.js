7cec32d6073dbc234e27ece0999b3f360ae39aa7
Glenn Dunnegan <glenn.dunnegan@gmail.com>
2026-02-23 12:47:51 -0600

    Updated while pre-rendering

--- scripts/copy404.js
+++ scripts/copy404.js
@@ -1,7 +1,11 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const distDir = path.resolve('dist');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const distDir = path.resolve(__dirname, '../dist');
 const src = path.join(distDir, 'index.html');
 const dest = path.join(distDir, '404.html');
 
