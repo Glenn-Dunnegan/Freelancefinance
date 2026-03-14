1aea88b3dae944b92c6301cf0624bcf7dec9364d
Glenn Dunnegan <glenn.dunnegan@gmail.com>
2026-03-13 21:19:35 -0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-  declare global {
-    interface Window {
-      gtag?: (...args: unknown[]) => void;
-    }
-  }
-
-  createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
   
